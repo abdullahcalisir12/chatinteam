5361b2a247cdb1cd096ef0238d2e0389233ed9b6
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-11-05 16:30:56 +0300

    user service updated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,16 +9,35 @@ export class UserService {
     private prisma: PrismaService
   ) {}
   async findOne(where: UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.findOne({ where });
+    try {
+      const user = this.prisma.user.findOne({ where });
+      if(!user) throw new Error('User Not Found');
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async create(data: UserCreateInput): Promise<User> {
-    return this.prisma.user.create({ data });
+    try {
+      const createdUser = this.prisma.user.create({ data });
+      if (!createdUser) throw new Error('Failed to create user');
+      return createdUser;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async delete(where: UserWhereUniqueInput): Promise<DeleteResult> {
-    const user = await this.prisma.user.delete({ where });
-    if (!user) throw new Error('Error');
-    return { id: where.id };
+    try {
+      const user = this.findOne(where);
+      if (!user) throw new Error('User not found');
+
+      const deletedUser = await this.prisma.user.delete({ where });
+      if (!deletedUser) throw new Error('Failed to delete user');
+      return { id: where.id };
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
